ea4d6b57c92f06a67ac3e5ac9a8b3bd3ffc32c19
kishanpadhiyar44 <85817901+kishanpadhiyar44@users.noreply.github.com>
2022-07-09 12:03:53 +0530

    Add files via upload

---
+++ Bookstore_Web/BookStore.Api/BookStore.Models/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Models.ViewModels;
+
+namespace BookStore.Models.Models
+{
+    public class UserModel
+    {
+        public UserModel() { }
+
+        public UserModel(UserModel user)
+        {
+            Id = user.Id;
+            Firstname = user.Firstname;
+            Lastname = user.Lastname;
+            Email = user.Email;
+            Roleid = user.Roleid;
+        }
+
+        public UserModel(User user)
+        {
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public int Roleid { get; set; }
+    }
+}
---
+++ Bookstore_Web/BookStore.Api/BookStore.Models/ViewModels/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.Models.ViewModels
+{
+    public partial class User
+    {
+        public User()
+        {
+            Carts = new HashSet<Cart>();
+            Ordermsts = new HashSet<Ordermst>();
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Roleid { get; set; }
+
+        public virtual Role Role { get; set; }
+        public virtual ICollection<Cart> Carts { get; set; }
+        public virtual ICollection<Ordermst> Ordermsts { get; set; }
+    }
+}
