5192309ff120841b17c3a071c4ddacd47f216d3d
kishanpadhiyar44 <85817901+kishanpadhiyar44@users.noreply.github.com>
2022-07-09 12:07:18 +0530

    Add files via upload

---
+++ Bookstore_Web/BookStore.Api/BookStore.Repository/CartRepository.cs
@@ -0,0 +1,65 @@
+﻿using BookStore.Models.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class CartRepository : BaseRepository
+    {
+        
+        public ListResponse<Cart> GetCart(int userId)
+        {
+            var query = _context.Carts.Where(c => c.Userid == userId);
+            int totalRecords = query.Count();
+            List<Cart> cart = query.ToList();
+
+            foreach (Cart cartItem in cart)
+            {
+                cartItem.Book = _context.Books.FirstOrDefault(c => c.Id == cartItem.Bookid);
+            }
+
+            return new ListResponse<Cart>()
+            {
+                Results = cart,
+                TotalRecords = totalRecords,
+            };
+        }
+
+        public Cart GetCarts(int id)
+        {
+            return _context.Carts.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Cart AddCart(Cart category)
+        {
+            var entry = _context.Carts.Add(category);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public Cart UpdateCart(Cart category)
+        {
+            var entry = _context.Carts.Update(category);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public bool DeleteCart(int id)
+        {
+            var cart = _context.Carts.FirstOrDefault(c => c.Id == id);
+            if(cart == null)
+            {
+                return false;
+            }
+
+            _context.Carts.Remove(cart);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ Bookstore_Web/BookStore.Api/BookStore.Repository/UserRepository.cs
@@ -0,0 +1,94 @@
+﻿using BookStore.Models.ViewModels;
+using BookStore.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class UserRepository
+    {
+        BookStoreContext _context = new BookStoreContext();
+
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User Login(LoginModel model) => _context.Users.FirstOrDefault(c => c.Email.Equals(model.Email) && c.Password.Equals(model.Password));
+
+        public User Register(RegisterModel model)
+        {
+            User user = new User()
+            {
+                Email = model.Email,
+                Password = model.Password,
+                Firstname = model.Firstname,
+                Lastname = model.Lastname,
+                Roleid = model.Roleid,
+            };
+            var entry = _context.Users.Add(user);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public ListResponse<User> GetUsers(int pageIndex, int pageSize, string keyword)
+        {
+            keyword = keyword?.ToLower().Trim();
+            var query = _context.Users.Where(c 
+                 => keyword == null 
+                 ||c.Firstname.ToLower().Contains(keyword) 
+                 || c.Lastname.ToLower().Contains(keyword)
+            ).AsQueryable();
+
+            int totalRecords = query.Count();
+            IEnumerable<User> users = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            return new ListResponse<User>()
+            {
+                Results = users,
+                TotalRecords = totalRecords
+            };
+        }
+
+        public ListResponse<Role> GetRoles(RoleModel role)
+        {
+            var query = _context.Roles;
+            int totalRecords = query.Count();
+            List<Role> roles = query.ToList();
+            return new ListResponse<Role>()
+            {
+                Results = roles,
+                TotalRecords = totalRecords,
+            };
+        }
+
+        public User GetUser(int id)
+        {
+            return _context.Users.FirstOrDefault(w => w.Id == id);
+        }
+
+        public User UpdateUser(User model)
+        {
+            var entry = _context.Update(model);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public bool DeleteUser(int id)
+        {
+            User user = _context.Users.FirstOrDefault(w => w.Id == id);
+            if (user == null)
+                return false;
+
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+
+    }
+}
